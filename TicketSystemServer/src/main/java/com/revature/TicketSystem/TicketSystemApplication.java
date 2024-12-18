a06b804287923ad287d5d9d85257571c4d230505
cheffuller <cheffuller@gmail.com>
2024-12-18 11:16:16 -0600

    enabling logging

--- TicketSystemServer/src/main/java/com/revature/TicketSystem/TicketSystemApplication.java
+++ TicketSystemServer/src/main/java/com/revature/TicketSystem/TicketSystemApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TicketSystemApplication.class, args);
+
 	}
 
 }
--- TicketSystemServer/src/test/java/com/revature/TicketSystem/TicketSystemApplicationTests.java
+++ TicketSystemServer/src/test/java/com/revature/TicketSystem/TicketSystemApplicationTests.java
@@ -3,6 +3,7 @@ package com.revature.TicketSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class TicketSystemApplicationTests {
 
